f4f9c66f879e12da9cf9e8c2c1d9adbe44d338f8
JULIAN CARABALLO <46603069@ort>
2023-05-10 08:58:20 -0300

    empezando

---
+++ src/Titulos.js
@@ -0,0 +1,13 @@
+import './Titulos.css';
+
+function Titulos() {
+  return (
+    <div className="Titulos">
+      <h1 className="titulo1">Administrador de Pacientes</h1>
+      <h2 className="titulo2">Crear mi cita</h2>
+      <h2 className="titulo3">Administrar tus cita</h2>  
+    </div>
+  );
+}
+
+export default Titulos;
